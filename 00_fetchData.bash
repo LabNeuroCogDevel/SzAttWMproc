b1c2f8624d945e1e9ec4f6f4ebbbbf293520175b
Laboratory of Neurocognitive Development <willforan+lncd@gmail.com>
2014-10-20 15:47:38 -0400

    added comments

--- 00_fetchData.bash
+++ 00_fetchData.bash
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+
+# rsync is a file transfer program capable of efficient remote update via a fast differencing algorithm#-v=verbose#-r=recursive into directory#-z=compress file data during transfer#-h=output numbers in a human readable format#Will is getting the data from Wallace and copying it to Phillips 
 rsync -rvzhi foranw@wallace:/data/Luna1/Raw/P5Sz/ ./subj/
