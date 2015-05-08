256f27c99c28f193e7761bb71f25b5d3e8e3d464
Laboratory of Neurocognitive Development <willforan+lncd@gmail.com>
2015-05-08 15:44:34 -0400

    add redo, add fm, add finalpfix, fix tr compare

--- scripts/02_preproc.bash
+++ scripts/02_preproc.bash
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-# stop runnign script if any errors
-set -e
-
-CFGFILE="$(dirname $0)/jobs_cfg.bash"
-# pull in waitForJobs function
-#  function takes a config file as settings argument
-source $CFGFILE
+# USAGE:
+# ./02_preproc.bash      # run everyone who's incomplete
+# ./02_preproc.bash show # show don't do
+# REDO=1 02_preproc.bash # redo everyone (useful if preprocessOne.bash changes)
 
 # preprocess Multiband functionals
 #
-# run like `02_preproc.bash show` to see what it will do (and not do it)
 # 
 # * will silently skip if expected final output nfswdktm_${protocol}_5.nii.gz exists already
 # * will warn and retry if preproc dir exists but does not have final output
@@ -19,6 +15,19 @@ source $CFGFILE
 #      but because both this and cpMprage will run preprocessMprage
 #      -- could be race condition where file is being written twice
 #      so always cpMprage and then preprocess functionals
+
+
+# stop runnign script if any errors
+set -e
+
+#what does the final output look like
+finalpfix="nfswdktm"
+
+CFGFILE="$(dirname $0)/jobs_cfg.bash"
+# pull in waitForJobs function
+#  function takes a config file as settings argument
+source $CFGFILE
+
 
 # we need matlab for preprocessFunctional's wavelet despiking
 if ! which matlab >/dev/null; then echo "matlab needs to be in the path!" && exit 1; fi
@@ -49,24 +58,25 @@ while read MBimg; do
 	savedir=${proto}_$num
 
 	# skip if we already have preproccesed directory
-	[ -r subj/$subj/preproc/$savedir/nfswdktm_${savedir}_5.nii.gz ] && continue
+	# and we dont want to redo
+	[ -z "$REDO" && -r subj/$subj/preproc/$savedir/$finalpfix_${savedir}_5.nii.gz ] && continue
 
 	skipmefile="subj/$subj/preproc/$savedir/skipme"
 	[ -r "$skipmefile" ] && echo "skiping $subj $savedir ($skipmefile)" && continue
 	# if we have the directory but not the final file, something went wrong before
-	[ -d subj/$subj/preproc/$savedir ] && \
+	[ -z "$REDO" && -d subj/$subj/preproc/$savedir ] && \
 	       	echo "* WARNING: $subj $savedir folder exists but not final preproc, trying to redo now!" && \
 		echo "           'date > $skipmefile'  to skip"
 
 
 	# if we provided an argument to the script, just show what needs to be done
 	if [ -n "$1" ]; then
-		echo scripts/preprocessOne.bash $subj $fbase $savedir 
+		echo REDO=\"$REDO\" scripts/preprocessOne.bash $subj $fbase $savedir 
 		continue
 	fi
 
 	echo "$subj $fbase -> $savedir"
-        scripts/preprocessOne.bash $subj $fbase $savedir &
+        REDO="$REDO" scripts/preprocessOne.bash $subj $fbase $savedir &
 	# wait if we have too many jobs going
 	waitForJobs $CFGFILE
 done
--- scripts/preprocessOne.bash
+++ scripts/preprocessOne.bash
@@ -12,11 +12,18 @@
 
 # run like: ./preprocessOne.bash btc_08222014 attention_1
 # 
+# to redo even if we already have final output
+# REDO=1 ./preprocessOne.bash btc_08222014 attention_1
 
+# where can we find warping output
 warp=mprage_warpcoef.nii.gz
 bet=mprage_bet.nii.gz
+# what defines the slice acquisition sequenction
 timing1d=/Volumes/Phillips/P5/scripts/sliceTimings.1D
+# actual TR, 3dcopy of hdr doesn't maintain TR in header
 TR=1.0
+# what does the final nii.gz start with
+finalpfix="nfswdktm"
 
 scriptdir=$(cd $(dirname $0);pwd)
 subjdir=$(cd $scriptdir/../subj;pwd)
@@ -43,9 +50,27 @@ if [ -n "$3" ]; then savename=$3; else savename=$runname; fi
 [ -z "$savename" ] && echo "no savename (2nd or 3rd argument)" && exit 1
 
 #looking for the run file inside /Volumes/Phillips/P5/subj/11327_20140911
-run=$(ls $sdir/MB/*$runname*hdr|grep -v ref|tail -n1)
-[ ! -r "$run" ] && echo "cannot find run dir ($run)" && exit 1
+#run=$(ls $sdir/MB/*$runname*hdr|grep -v ref|tail -n1)
+run=$(find  $sdir/MB/ -maxdepth 1 -iname "*$runname*hdr" -and -not -iname '*ref.hdr'|tail -n1)
+[ -z "$run" -o ! -r "$run" ] && echo "cannot find run.hdr  ($sdir/MB/*$runname*hdr)" && exit 1
 
+
+## ref image
+refimg=$(find  $sdir/MB/ -maxdepth 1 -iname "*$runname*ref.hdr"|tail -n1)
+[ -z "$refimg" -o ! -r "$refimg" ] && echo "cannot find run_ref.hdr  ($sdir/MB/*runname*_ref.hdr)" && exit 1
+
+## Field maps
+# first mag then phase
+# -fm_magnitude "/Volumes/Phillips/P5/subj/$s2/gre_field_mapping_new_96x90.13/MR*" 
+# -fm_phase "/Volumes/Phillips/P5/subj/$s2/gre_field_mapping_new_96x90.14/MR*" 
+fms=( $( find $sdir -maxdepth 1 -iname 'gre_field_mapping_new_96x90.*' |tail -n2  ) )
+[ ${#fms[@]} -ne 2 ] && echo "cannot find mag and phase in $sdir/gre_field_mapping_new_96x90.*" && exit 1
+
+fm_mag=${fms[0]}
+fm_phase=${fms[1]}
+# check we have the right directories (based on dicom count)
+[ $(ls $fm_mag/MR*  |wc -l) -ne 120 ] && echo "fm_mag does not have 120 MRs! ($fm_mag)"   && exit 1
+[ $(ls $fm_phase/MR*|wc -l) -ne  60 ] && echo "fm_mag does not have  60 MRs! ($fm_phase)" && exit 1
 
 set -xe
 
@@ -73,22 +98,40 @@ ppdir=$sdir/preproc/$savename
 [ ! -d $ppdir ] && mkdir -p  $ppdir
 cd $ppdir
 
-# test we haven't done this before
-#fc=$(find . -name 'nfswdktm_*.nii.gz'|wc -l)
-#[[ "$fc" -ge "2" ]] && echo "already have nfswdktm_*, consider removing $(pwd)/nfswdktm_* before running $0 $@" && exit 1
+# test we haven't done this before if we dont explicitly say to REDO
+if [ -z "$REDO" ]; then
+  fc=$(find . -name "$finalpfix_*.nii.gz"|wc -l)
+  [[ "$fc" -ge "2" ]] && echo "already have nfswdktm_*, consider 
+  REDO=1 $0 $@ 
+or removing $(pwd)/nfswdktm_*" && exit 1
+fi
 
 # move from ANALYZE to niifty
-#[ ! -r $savename.nii.gz ] && 3dcopy $run $savename.nii.gz
+[ ! -r $savename.nii.gz ] && 3dcopy $run $savename.nii.gz
 
 # reset TR
 TRreported=$(3dinfo -tr $savename.nii.gz)
-#[[ "$TRreported" != "$TR" ]] &&  3drefit -TR $TR $savename.nii.gz
+diff=$(echo "$TRreported - $TR" | bc -l )
+# can not use -eq 0 becuase diff might be non-digit (e.g. ".5")
+[[ "$diff" != "0" ]] &&  3drefit -TR $TR $savename.nii.gz
 
 find . -mindepth 1 -maxdepth 1 -newer .motion_correction_complete -exec rm -r {} \;
 
 # actually preproc
 echo "preproc $s $savename ($ppdir)"
-preprocessFunctional -4d $savename.nii.gz -tr $TR -mprage_bet $mpragedir/$bet -warpcoef $mpragedir/$warp -threshold 98_2 -hp_filter 100 -rescaling_method 10000_globalmedian -template_brain MNI_2.3mm -func_struc_dof bbr -warp_interpolation spline -constrain_to_template y -wavelet_despike -wavelet_m1000 -wavelet_threshold 10 -4d_slice_motion -custom_slice_times $timing1d -mc_movie -motion_censor fd=0.9,dvars=20 -nuisance_file nuisance_regressors.txt -nuisance_compute csf,dcsf,wm,dwm -func_refimg "/Volumes/Phillips/P5/subj/$s2/MB/ep2d_MB_attention_X1_twix_ref.hdr" -fm_phase "/Volumes/Phillips/P5/subj/$s2/gre_field_mapping_new_96x90.14/MR*" -fm_magnitude "/Volumes/Phillips/P5/subj/$s2/gre_field_mapping_new_96x90.13/MR*" -fm_cfg clock
+
+preprocessFunctional -4d $savename.nii.gz -tr $TR \
+	 -mprage_bet $mpragedir/$bet -warpcoef $mpragedir/$warp \
+	 -threshold 98_2 -hp_filter 100 \
+	 -rescaling_method 10000_globalmedian \
+	 -template_brain MNI_2.3mm -func_struc_dof bbr -warp_interpolation spline \
+	 -constrain_to_template y -wavelet_despike -wavelet_m1000 -wavelet_threshold 10 \
+	 -4d_slice_motion -custom_slice_times $timing1d -mc_movie \
+	 -motion_censor fd=0.9,dvars=20 -nuisance_file nuisance_regressors.txt \
+	 -nuisance_compute csf,dcsf,wm,dwm \
+	 -func_refimg $refimg \
+	 -fm_phase "$fm_phase/MR*" -fm_magnitude "$fm_mag/MR*" \
+	 -fm_cfg clock
 
 
 # everything is okay!
