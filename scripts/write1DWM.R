7f5fc28dbc1644a2bd85b3847ee4326d04c0ac3f
Laboratory of Neurocognitive Development <willforan+lncd@gmail.com>
2016-04-04 15:50:50 -0400

    track ProbeRT20160225 1d function

--- scripts/write1DWM.R
+++ scripts/write1DWM.R
@@ -5,7 +5,7 @@ source('readWMmat.R')
 
 #####################
 # function to write out 1D files (in curent working directory)
-#
+# if you want to make a new set of 1D files you need to make new
 # pass to writeAll1DsWith , which will handle finding subjects, loading visit, and cd-ing)
 # our run in subject 1D directory and load subjects visit yourself
 #
@@ -37,6 +37,39 @@ fulltrial20160223 <- function(visit) {
 
 }
 
+
+
+# N.B. We are overwriting some of the matlab script
+# MATLAB SCRIPT SHOULD BE RUN FIRST! 
+# this does not provided catch + cue combined
+# just partionals for incorrect cue, delay, and all of modulated probe
+# OVERWERITES probe_ld*_correct
+# in correct_load_wrongtogether_dlymod
+ProbeRT20160225 <- function(visit) {
+
+  # cue - wrong (same dur as cue correct)
+  visit$cuedur <- visit$mem - visit$cue
+  subset(visit,Correct %in% c(-1,0)) %>% save1D('cue',dur='cuedur',"cueonly_incorrect.1D") 
+
+  # delay - wrong (same as dur delya correct)
+  visit$dlydur <- visit$probe - visit$delay
+  subset(visit,Correct %in% c(-1,0)) %>% save1D('delay',dur='dlydur',"dlyonly_incorrect.1D") 
+
+  ## probe wrong (depend on RT, no resp)
+  # probe
+  visit$probedur <- ifelse(visit$response>0, 
+                       visit$response - visit$probe,
+                       visit$finish   - visit$probe)
+
+  # correct
+  subset(visit,Correct==1&load==1)   %>% save1D('probe',dur='probedur',"probe_ld1_correct.1D") 
+  subset(visit,Correct==1&load %in% c(3,4) )   %>% save1D('probe',dur='probedur',"probe_ld3_correct.1D") 
+
+  subset(visit,Correct %in% c(-1,0)) %>% save1D('probe',dur='probedur',"probe_incorrect.1D") 
+
+}
+isit <- readVisit(findSubjMat('11500_20160215'))
+
 # USAGE:
 #   #MANUAL:
 #   # load data
@@ -58,4 +91,13 @@ fulltrial20160223 <- function(visit) {
 
 ############
 # DO ALL
-writeAll1DsWith(fulltrial20160223,outdir='R1D')
+# first pass
+#writeAll1DsWith(fulltrial20160223,outdir='R1D')
+# overwrite probe correct from matlab
+# add cue and delay incorrect
+writeAll1DsWith(ProbeRT20160225,outdir='correct_load_wrongtogether_dlymod')
+
+# run just for 11333
+visit_load4 <- readVisit(findSubjMat('sm-20140829'))
+setwd('/Volumes/Phillips/P5/subj/sm-20140829/1d/WM/correct_load_wrongtogether_dlymod/')
+ProbeRT20160225(visit_load4)
